0943f5724489e1cca6f1a4420de57c0c7e02c0d3
rofe-mk <robert.fehrmann@mkpayment.com>
2015-04-13 11:16:27 +0000

    fix template name

--- attributes/default.rb
+++ attributes/default.rb
@@ -61,12 +61,12 @@ default['kibana']['config']['elasticsearch'] = "window.location.protocol+\"//\"+
 
 # nginx variables
 default['kibana']['nginx']['install_method'] = 'package'
-default['kibana']['nginx']['template'] = 'kibana-nginx.conf.erb'
+default['kibana']['nginx']['template'] = 'kibana-nginx_git.conf.erb'
 default['kibana']['nginx']['template_cookbook'] = 'kibana_lwrp'
 default['kibana']['nginx']['enable_default_site'] = false
 default['kibana']['nginx']['install_method'] = 'package'
 
 # Apache variables.
-default['kibana']['apache']['template'] = 'kibana-apache.conf.erb'
+default['kibana']['apache']['template'] = 'kibana-apache_git.conf.erb'
 default['kibana']['apache']['template_cookbook'] = 'kibana_lwrp'
 default['kibana']['apache']['enable_default_site'] = false
